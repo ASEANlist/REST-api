d02e9a767e396143e21cbe58dc77bfb96ce911a5
NamPE286 <nambuihung654@gmail.com>
2024-05-03 12:02:24 +0700

    type

--- src/utils/level/getLevel.ts
+++ src/utils/level/getLevel.ts
@@ -1,5 +1,6 @@
 import { pb } from "@src/database/pocketbase";
+import type { LevelsViewRecord } from "@src/types/pocketbase";
 
-export default async function(id: number) {
+export default async function(id: number): Promise<LevelsViewRecord> {
     return await pb.collection('levels_view').getFirstListItem(`levelID=${id}`);
 }
