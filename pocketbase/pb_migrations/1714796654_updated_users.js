7b314ae991852c4202ea3e8b6a404a92a4c0811d
NamPE286 <nambuihung654@gmail.com>
2024-05-04 11:30:38 +0700

    update

---
+++ pocketbase/pb_migrations/1714796654_updated_users.js
@@ -0,0 +1,44 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "2miq3tij",
+    "name": "country",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": 2,
+      "max": 2,
+      "pattern": "\\b[A-Z]+\\b"
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "2miq3tij",
+    "name": "country",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": 2,
+      "max": 2,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
